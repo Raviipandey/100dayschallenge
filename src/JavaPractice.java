a0fe9c0c5f6841b35df78ad9716e5956332e4768
ravit <rshanker084@gmail.com>
2021-09-14 00:04:52 +0530

    100 Days challenge for java

---
+++ src/JavaPractice.java
@@ -0,0 +1,12 @@
+public class JavaPractice {
+
+        public static void main(String[] args){
+            int a = 10;
+            int b = 15;
+            int sum;
+            sum = a + b;
+            System.out.println("The sum of a and b is "+sum);
+
+
+    }
+}
