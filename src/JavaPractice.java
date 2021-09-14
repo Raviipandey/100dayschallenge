4e2e89b3ce7b3884be590bb30255db9dc0a69aef
ravit <rshanker084@gmail.com>
2021-09-14 21:16:09 +0530

    Multiplication table using java

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -1,11 +1,9 @@
 public class JavaPractice {
-
+// Multiplication table using java
         public static void main(String[] args){
-            int a = 10;
-            int b = 15;
-            int sum;
-            sum = a + b;
-            System.out.println("The sum of a and b is "+sum);
+            int a ;
+            for(a=1; a<=10; a++)
+            System.out.println("8 x "+a + " = " +a*8);
 
 
     }
