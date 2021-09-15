a41e718213e6c6298079f225dae9004e24d5b56a
ravit <rshanker084@gmail.com>
2021-09-16 01:02:22 +0530

    Calculator program

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -1,10 +1,16 @@
+
 public class JavaPractice {
-// Multiplication table using java
+// Calculator progra
         public static void main(String[] args){
-            int a ;
-            for(a=1; a<=10; a++)
-            System.out.println("8 x "+a + " = " +a*8);
-
-
+            int a=24,b=8,sum,diff,prod,div;
+            sum = a+b;
+            diff = a-b;
+            prod = a*b;
+            div = a/b;
+            System.out.println("*****Welcome to calculator program*****");
+            System.out.println("The sum of " +a + " and " +b +  " is " +sum);
+            System.out.println("The difference of "+a + " and " +b +  " is " +diff);
+            System.out.println("The product of "+a + " and " +b +  " is " +prod);
+            System.out.println("The division of "+a + " and " +b +  " is " +div);
     }
 }
