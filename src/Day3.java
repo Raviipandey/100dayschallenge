bba9945edc9a24c8a3447d375ddf9bd913b6545a
ravit <rshanker084@gmail.com>
2021-09-18 02:22:17 +0530

    Taking input and printing it

---
+++ src/Day3.java
@@ -0,0 +1,13 @@
+/* Printing the given input */
+
+import java.util.*;
+public class Day3 {
+    public static void main(String[] args){
+        Scanner op = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = op.nextLine();
+        System.out.println("Hello "+name+", how are you ?");
+
+
+    }
+}
