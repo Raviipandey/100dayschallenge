2a17f2a393e38aa4c7d8698074a5947cc3eec000
ravit <rshanker084@gmail.com>
2021-09-17 19:54:43 +0530

    Odd even Program

---
+++ src/Day2.java
@@ -0,0 +1,11 @@
+public class Day2 {
+    public static void main(String args[]){
+        int num = 10;
+
+        if ( num % 2 == 0 )
+            System.out.println("The given number is even");
+        else
+            System.out.println("The given number is odd");
+
+    }
+}
