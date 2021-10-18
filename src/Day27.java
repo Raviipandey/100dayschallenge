3b093c33a1e8bd0fea848d57f7daf640d8c4e651
Ravi Pandey <89774714+Raviipandey@users.noreply.github.com>
2021-10-18 11:52:31 +0530

    Add files via upload

---
+++ src/Day27.java
@@ -0,0 +1,15 @@
+/* Concatenation Of Strings */
+
+import java.util.*;
+public class Day27 {
+    public static void main(String[] args) {
+        // concatenation
+        String Firstname = "Ravi";
+        String Middlename = "Shankar";
+        String Lastname = "Pandey";
+        String Fullname = Firstname  + " " + Middlename + " " + Lastname ;
+        System.out.println(Fullname);
+
+
+    }
+}
