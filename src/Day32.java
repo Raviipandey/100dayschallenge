ec410c143d179b9d7fa3fdd2fc184a8fd39934b8
Ravi Pandey <89774714+Raviipandey@users.noreply.github.com>
2021-11-06 01:20:07 +0530

    Add files via upload

---
+++ src/Day32.java
@@ -0,0 +1,10 @@
+/* Concatenation of strings using concat() function */
+import java.util.*;
+public class Day32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String FirstName = "Vincenzo ";
+        String SecondName = "Cassano";
+        System.out.println(FirstName.concat(SecondName));
+    }
+}
